4b59a21f3437099e308c49b5dbfad78b108e7609
Bhanu Pratap Singh <bhanuprataps1610@gmail.com>
2025-08-24 10:53:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from genetic_algorithm import *
 from simulate import *
+from model import *
 
-POP_SIZE = 5000
-GENERATIONS = 70
+POP_SIZE = 500
+GENERATIONS = 30
 INDIVIDUAL_SIZE = 24 * 18 + 18 * 18 + 54
 np.random.seed(42)
-population = [np.random.uniform(-1, 1, INDIVIDUAL_SIZE) for _ in range(POP_SIZE)]
+last_gen, population, fitness_scores = load_last_generation()
+if last_gen is not None:
+    start_gen = last_gen + 1
+else:
+    start_gen = 0
+    population = [np.random.uniform(-1, 1, INDIVIDUAL_SIZE) for _ in range(POP_SIZE)]
+
 
 for gen in range(GENERATIONS):
-    fitness_scores = []
     scores = []
-    print(f"Generation : {gen}")
+    fitness_scores = []
+    print(f"Generation : {gen+start_gen}")
     for i, individual in enumerate(population):
         fitness, score = simulate(individual)
         fitness_scores.append(fitness)
@@ -18,6 +25,9 @@ for gen in range(GENERATIONS):
         if i% 100 == 0:
             print(i," : ",fitness)
     print(sum(fitness_scores)/POP_SIZE)
+
+    if (gen + start_gen) % 5 == 0:
+        save_generation(start_gen + gen, population.copy(), fitness_scores.copy())
     
     best_fitness_ind = np.argmax(fitness_scores)
     print("Best fitness : ", fitness_scores[best_fitness_ind], "Score : ", scores[best_fitness_ind])
